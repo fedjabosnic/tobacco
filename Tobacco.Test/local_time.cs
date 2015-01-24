258fe2bf9ace430c8a7025f3bb1dc738c8108154
Fedja Bosnic <fedjabosnic@gmail.com>
2015-01-24 23:30:29 +0000

    Added unit tests

---
+++ Tobacco.Test/local_time.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tobacco.Test
+{
+	[TestClass]
+	public class local_time
+	{
+		[TestMethod]
+		public void should_return_a_local_datetime()
+		{
+			var time = new Time();
+
+			time.Local.Kind.Should().Be(DateTimeKind.Local);
+		}
+	}
+
+	[TestClass]
+	public class universal_time
+	{
+		[TestMethod]
+		public void should_return_a_utc_datetime()
+		{
+			var time = new Time();
+
+			time.Universal.Kind.Should().Be(DateTimeKind.Utc);
+		}
+	}
+
+	[TestClass]
+	public class localized_time
+	{
+		[TestMethod]
+		public void should_return_an_unspecified_datetime()
+		{
+			var time = new Time();
+
+			time.Localized(TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time")).Kind.Should().Be(DateTimeKind.Unspecified);
+		}
+	}
+}
