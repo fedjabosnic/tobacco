a77f5e53c82e21a5c633f36c097d665c821ab7ec
Fedja Bosnic <fedjabosnic@gmail.com>
2015-01-24 16:33:20 +0000

    Added project skeleton
    - Introduced 'environment'
    - Introduced 'time'

---
+++ Tobacco/Environment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tobacco
+{
+	public static class Environment
+	{
+		public static ITime Time { get; set; }
+	}
+
+	public interface ITime
+	{
+		DateTime Local { get; }
+		DateTime Universal { get; }
+		DateTime Localized(TimeZoneInfo timezone);
+	}
+
+	public class Time : ITime
+	{
+		public DateTime Local { get { return DateTime.Now; } }
+		public DateTime Universal { get { return DateTime.UtcNow; } }
+		public DateTime Localized(TimeZoneInfo timezone) { return TimeZoneInfo.ConvertTimeBySystemTimeZoneId(Universal, TimeZoneInfo.Utc.Id, timezone.Id); }
+	}
+}
