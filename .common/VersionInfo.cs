22d923d6c59d2b3e27544dbec8c4a91137518394
Fedja Bosnic <fedjabosnic@gmail.com>
2015-01-25 13:32:55 +0000

    Introduced shared version info file
    - Added shared version info file to solution and referenced it from projects
    - Cleaned up project assembly info files in each project
    - Modified appvoyer.yml file to update the version info file on builds

---
+++ .common/VersionInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Fedja Bosnic")]
+[assembly: AssemblyCopyright("Copyright © Fedja Bosnic 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Assembly version information:
+//
+// FORMAT:   [Major].[Minor].[Revision].[Build]
+// Major:    Breaking changes to public interface
+// Minor:    Non-breaking changes to public interface
+// Revision: Internal changes (not affecting public interface)
+// Build:    Incrementing build number (automatic)
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
