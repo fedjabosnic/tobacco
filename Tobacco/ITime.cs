3eecf5b7316116ff966c4f9c2bb9eb881dd94e2b
Fedja Bosnic <fedjabosnic@gmail.com>
2015-02-01 16:28:54 +0000

    Added virtual and actual implementations of time
    - Added actual time implementation
      - Uses system calls to get local, universal and localized time
    - Added virtual time implementation
      - Allows setting local and universal time
      - Localized time cannot currently be mocked
    - Refactored and reorganised project structure

---
+++ Tobacco/ITime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tobacco
+{
+	public interface ITime
+	{
+		/// <summary>
+		/// Returns the current local time of the hosting machine/os (equivalent to DateTime.Now)
+		/// </summary>
+		DateTime Local { get; }
+		/// <summary>
+		/// Returns the current universal time irregardless of the hosting machine/os (equivalent to DateTime.UtcNow).
+		/// </summary>
+		DateTime Universal { get; }
+		/// <summary>
+		/// Returns the current time localized to the specified timezone.
+		/// </summary>
+		DateTime Localized(string timezone);
+	}
+}
---
+++ Tobacco/Time/Actual.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tobacco.Time
+{
+	public class Actual : ITime
+	{
+		public DateTime Local
+		{
+			get
+			{
+				return DateTime.Now;
+			}
+		}
+
+		public DateTime Universal
+		{
+			get
+			{
+				return DateTime.UtcNow;
+			}
+		}
+
+		public DateTime Localized(string timezone)
+		{
+			return TimeZoneInfo.ConvertTimeBySystemTimeZoneId(Universal, TimeZoneInfo.Utc.Id, timezone);
+		}
+	}
+}
